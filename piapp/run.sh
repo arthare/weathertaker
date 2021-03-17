a3e7faeaee757c7a3abcf117e0b6f94dd66a2477
art hare <art.hare@4iiii.com>
2021-03-16 21:06:36 -0600

    more additions to make sure startup.txt exists

--- piapp/run.sh
+++ piapp/run.sh
@@ -7,7 +7,9 @@ cd ../webapp
 npm install
 popd
 
+echo "startup!" > ./tmp/startup.txt
 /opt/nodejs/bin/tsc
+echo "startup!" > ./tmp/startup.txt
 /opt/nodejs/bin/pm2 restart pm2.json
 /opt/nodejs/bin/pm2 restart pm2-watchdog.json
 
