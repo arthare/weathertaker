8d99fc6303452daf840807629ce680197aaa7b62
art <art@wifilapperhome>
2021-03-16 21:05:22 -0600

    More logging changes

--- piapp/index-watchdog.ts
+++ piapp/index-watchdog.ts
@@ -6,12 +6,17 @@ export async function runWatchdog() {
   // why?  raspistill seems to have a nasty probability of locking up about once a day.  If the actual camera process locks up, 
   // this simple process can reboot the pi and we'll only miss a couple minutes of photos.
   const stats = fs.lstatSync('./tmp/from-camera.jpg');
+  const statSignal = fs.lstatSync('./tmp/startup.txt');
+  const tmStarted = statSignal.mtimeMs;
   const tmMod = stats.mtimeMs;
   const tmNow = new Date().getTime();
 
-  console.log(`Watchdog: from-camera.jpg was modified ${((tmNow - tmMod)/1000).toFixed(1)}s ago`);
-  const msTimeLast = tmNow - tmMod;
-  if(msTimeLast > 90000) {
+  const msSinceLast = tmNow - tmMod;
+  const msSinceStartup = tmNow - tmStarted;
+
+  console.log(`Watchdog: from-camera.jpg was modified ${((msSinceLast)/1000).toFixed(1)}s ago`);
+  console.log(`Watchdog: startedup.txt was modified ${((msSinceStartup)/1000).toFixed(1)}s ago`);
+  if(msSinceLast > 90000 && msSinceStartup > 120000) {
     console.log("it's been too long, we should reboot!");
     execSync('sudo reboot');
   } else {
