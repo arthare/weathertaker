50883d014bfb495b150b9ca947bcbcb1d2e48f63
art <art@wifilapperhome>
2021-03-16 21:08:07 -0600

    Possibly making this handle pm2 being in different spots in an incredibly gross way

--- piapp/run.sh
+++ piapp/run.sh
@@ -11,6 +11,12 @@ mkdir -p ./tmp
 echo "startup!" > ./tmp/startup.txt
 /opt/nodejs/bin/tsc
 echo "startup!" > ./tmp/startup.txt
+
+echo "starting main app pm2"
 /opt/nodejs/bin/pm2 restart pm2.json
+pm2 restart pm2.json
+
+echo "starting watchdog app pm2"
 /opt/nodejs/bin/pm2 restart pm2-watchdog.json
+pm2 restart pm2-watchdog.json
 
