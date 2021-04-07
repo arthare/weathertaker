b2d3f313e9ca8eacbbcdca14827a50a27b620f51
art <art@wifilapperhome>
2021-04-06 20:34:59 -0600

    Logs in the webserver

--- piapp/index-webserver.ts
+++ piapp/index-webserver.ts
@@ -4,6 +4,18 @@ import fs from 'fs';
 import { takeImageAsap } from './index-pictureloop';
 
 export function setupLocalApi() {
+
+  let oldConsoleLog = console.log;
+  let lastConsoles:any[][] = [];
+  console.log = function() {
+    oldConsoleLog.apply(console, arguments);
+    lastConsoles.push([...arguments]);
+    while(lastConsoles.length > 300) {
+      lastConsoles.shift();
+    }
+  }
+
+
   let app = <core.Express>express();
   
   app.get('/last-image', (req:core.Request, res:core.Response) => {
@@ -14,6 +26,28 @@ export function setupLocalApi() {
       const file = process.cwd() + '/tmp/last-image.jpg';
       console.log("sending file ", file);
       res.sendFile(file);
+    } catch(e) {
+      console.error("Error at last-image: ", e);
+      res.writeHead(404, 'no-image');
+      res.write(JSON.stringify({err: e.message}));
+      res.end();
+    }
+  })
+  app.get('/logs', (req:core.Request, res:core.Response) => {
+
+    takeImageAsap();
+
+    try {
+      const file = process.cwd() + '/tmp/last-image.jpg';
+      
+      let lines = lastConsoles.map((lc) => {
+        return `<tr>` + lc.map((col) => `<td>${JSON.stringify(col)}</td>`) + `</tr>`;
+      });
+
+      let html = `<table style="width:90vw;" border=1>${lines.join('\n')}</table>`
+      res.write(html);
+      res.end();
+
     } catch(e) {
       console.error("Error at last-image: ", e);
       res.writeHead(404, 'no-image');
